ee85eea038a5256af1bbf549ff83c3e6cd9b8740
IvanEMF <imformaran@up.edu.ph>
2024-02-19 18:15:56 +0800

    chore: Updated index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+password1 = "iamaPassword1";
+password2 = "iamaPassword1";
+
+function validatePassword (password1,password2) {
+    x = password1.length;
+    y = password2.length
+
+    if (password1===password2 && x>=8 && x>=8) {
+
+        
+    }else{
+        return false;
+    }
+}
