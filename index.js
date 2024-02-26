1a1687fc5fb56e208cdaf401dd8f992403b027ce
IvanEMF <imformaran@up.edu.ph>
2024-02-26 14:30:22 +0800

    chore: Added reversePassword and storePassword functions

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-password1 = "iamapassword1";
-password2 = "iamapassword1";
+password1 = "iamaPassword1";
+password2 = "iamaPassword1";
 
 function validatePassword (password1,password2) {
     x = password1.length;
@@ -10,33 +10,65 @@ function validatePassword (password1,password2) {
 
     // checking conditions, if both passwords are the same and have at least 8 characters
     if (password1===password2 && x>=8 && y>=8) { 
-        
         for (var i=0; i<x; i++) {
+            z = password1.charAt(i);    //charAt lets you access the element of a string at a given value
             // checks if there is lowercase character
-            if (password1.charAt(i) >= 'a' && password1.charAt(i) <= 'z') { //no need to check 2nd password since they are same
+            if (z >= 'a' && z <= 'z') { //no need to check 2nd password since they are same
                 hasLowerChara = true;
             }
             // checks if there is uppercase character
-            if (password1.charAt(i) >= 'A' && password1.charAt(i) <= 'Z') {
+            if (z >= 'A' && z <= 'Z') {
                 hasUpperChara = true;
             }
             // checks if there is a number
-            if (password1.charAt(i) >= '0' && password1.charAt(i) <= '9') {
+            if (z >= '0' && z <= '9') {
                 hasNumber = true;
             }
         }
-        if (hasLowerChara == true && hasUpperChara == true && hasNumber == true) {
+        if (hasLowerChara===true && hasUpperChara===true && hasNumber===true) {
             return true;
         }else return false;
     }else return false;
 }
 
+// // testing validatePasswword function
+// if (validatePassword(password1,password2)) {
+//     console.log("True");
+// }else {
+//     console.log("False");
+// }
+
 function reversePassword (password) {
-    
+    x = password.length;
+    temp = "";
+
+    for (i = x-1; i >= 0; i--) {
+        // we are counting in reversse, so we can concatenate the temp string with the last elements
+        temp += password[i];
+    }
+    return temp;
+}
+// // testing reversePassword function
+// console.log(reversePassword(password1));
+
+function storePassword (name, password1, password2) {
+    // initialize new password to store for object
+    newPassword = "";
+    // check if passwords are valid
+    if (validatePassword(password1,password2)) {
+        //reverse password for new password
+        newPassword = reversePassword(password1);
+        // object to be returned
+        user = {
+            name: name,
+            password: newPassword
+        }
+        return user;
+    }else {
+        console.log("Wrong Password!");
+        return null;
+    }
 }
 
-if (validatePassword(password1,password2)) {
-    console.log("True");
-}else {
-    console.log("False");
-}
+// testing store password function
+console.log(storePassword("Ivan", password1,password2));
